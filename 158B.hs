c09c42c0d8e53aad525c9b77fe88e545b6c3f063
Michael Yan <michaelyan161@gmail.com>
2013-02-21 02:29:36 -0500

    158b too slow

--- 158B.hs
+++ 158B.hs
@@ -1,25 +1,23 @@
-import Data.List (sort)
+import Data.Sequence
 
 -- 158B
 taxi :: [Int] -> Int
-taxi [n] = 1
-taxi (n:ns) 
-	| n + last ns > 4 = 1 + taxi ns
-	| 
-	| n == 4 = 1 + taxi ns
-	| n == 3 = if 
-	where ns
-	
+taxi [a] = 1
+taxi [] = 0
+taxi ns@(x:xs)
+    | last xs <= (4-x) = taxi $ rotateAdd ns
+    | otherwise =  1 + taxi xs                
+    where rotateAdd (x:xs) = (x + last xs):(init xs)
 
 readIn :: IO [Int]
 readIn = do
     contents <- getContents
     let n:is = lines contents
         x = read $ head $ words n  
-        xs = take x $ map read is
-    return sort xs
+        xs = take x (map read (words $ head is))
+    return xs
 
 main :: IO()
 main = do
     i <- readIn
-    putStr $ show $ taxi i
+    putStr $ show $ taxi $ reverse $ sort i
